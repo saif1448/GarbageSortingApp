96353f86d4353c5fecaa871d0d382823a3825041
Saif Mahmud Parvez <mahmudalsaif@gmail.com>
2024-04-26 22:42:32 +0600

    v2

--- app/src/main/java/com/example/garbageapp/MainActivity.java
+++ app/src/main/java/com/example/garbageapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.garbageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,20 +12,32 @@ import com.example.garbageapp.utility.ItemsDB;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static ItemsDB itemsDB = ItemsDB.get();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button whereBtn = findViewById(R.id.where_btn);
+        Button addItemBtn = findViewById(R.id.add_item_btn);
         whereBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText itemText = findViewById(R.id.editTxt);
                 String itemName = itemText.getText().toString();
-                String place = itemName+ " should be placed in: "+ItemsDB.returnPlace(itemName.toLowerCase());
+                String place = itemName+ " should be placed in: "+itemsDB.returnPlace(itemName.toLowerCase());
                 itemText.setText(place);
             }
         });
+
+        addItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/garbageapp/utility/ItemsDB.java
+++ app/src/main/java/com/example/garbageapp/utility/ItemsDB.java
@@ -4,17 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemsDB {
-    private static List<Item> itemList = new ArrayList<>();
-    static {
-        itemList.add(new Item("milk carton", "Food"));
+    private static ItemsDB sItemsDB;
+    private List<Item> itemList = new ArrayList<>();
+    private ItemsDB(){
+        fillItemsDB();
     }
-    public static String returnPlace(String name){
+    public static ItemsDB get() {
+        if (sItemsDB == null) sItemsDB= new ItemsDB();
+        return sItemsDB;
+    }
+    public String returnPlace(String name){
         for(Item item: itemList){
             if(item.getName().equals(name)){
                 return item.getPlace();
             }
         }
         return "not found";
+    }
+
+    public void addItem(Item newItem){
+        itemList.add(newItem);
+    }
+
+    public void fillItemsDB() {
+        itemList.add(new Item("book1", "paper"));
+        itemList.add(new Item("book2", "paper"));
+        itemList.add(new Item("can", "metal"));
+        itemList.add(new Item("fish", "bio"));
+        itemList.add(new Item("meat", "bio"));
     }
 
 }
